ef62db5aff87941cfc6cf90cb0d227aa579dcfdb
kirthikagurumurthy <kirthikagurumurthy@gmail.com>
2022-06-07 10:50:15 -0700

    Scraping completed

---
+++ env/Scraper.py
@@ -0,0 +1,52 @@
+import json
+from time import mktime
+from datetime import datetime
+import feedparser as fp
+import newspaper
+from newspaper import Article
+
+MAX_ARTICLES = 10
+articles = {}
+articles['news_sites'] = {}
+ctr = 1
+
+with open('env/news_sites.json') as source:
+    news_sites = json.load(source)
+
+
+for news_site_name,val in news_sites.items():
+    news = newspaper.build(val['link'], memoize_articles=False)
+    all_news = {
+            "items": [],
+            "link": val['link']
+            
+        }
+  
+    for content in news.articles:
+        if ctr > MAX_ARTICLES:
+            break
+        try:
+            content.download()
+            content.parse()
+        except Exception as e:
+            print(e)
+            continue
+        if content.publish_date is None:
+            ctr = ctr + 1
+            continue
+        item = {}
+        item['title'] = content.title
+        item['link'] = content.url
+        item['text'] = content.text
+        item['published'] = content.publish_date.isoformat()
+        all_news['items'].append(item)
+        print(ctr, "News_Site:", news_site_name, "\n URL: ", content.url)
+        ctr = ctr + 1
+    
+
+    articles['news_sites'][news_site_name] = all_news
+    try:
+        with open('news_scraped.json', 'w') as op_file:
+            json.dump(articles, op_file)
+    except Exception as e: print(e)
+         
